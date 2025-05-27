3dfe85eab8929df0dbaf71608b6c11c49d218dee
zxyiled <sebastianmejiadev@gmail.com>
2025-05-27 01:55:09 -0500

    fixing some spaces in Person.java

--- src/main/java/org/odontoclin/logic/Person.java
+++ src/main/java/org/odontoclin/logic/Person.java
@@ -19,7 +19,7 @@ public class Person implements Serializable {
     @Temporal(TemporalType.DATE)
     private String dateOfBirth;
 
-    public Person(){
+    public Person() {
         //Void constructor
     }
 
@@ -36,39 +36,50 @@ public class Person implements Serializable {
     public int getId() {
         return id;
     }
+
     public void setId(int id) {
         this.id = id;
     }
+
     public String getDni() {
         return dni;
     }
+
     public void setDni(String dni) {
         this.dni = dni;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public String getLastName() {
         return lastName;
     }
+
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
     public String getPhone() {
         return phone;
     }
+
     public void setPhone(String phone) {
         this.phone = phone;
     }
+
     public String getAddress() {
         return address;
     }
+
     public void setAddress(String address) {
         this.address = address;
     }
+
     public String getDateOfBirth() {
         return dateOfBirth;
     }
